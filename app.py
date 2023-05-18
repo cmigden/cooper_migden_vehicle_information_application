8b83d1e2da5dc1e23868ac73667827d48e5d5cea
Cooper Migden <coopermigden@gmail.com>
2023-05-18 13:37:25 -0400

    updated filepath try 7

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import numpy as np
 # In[2]:
 
 
-data = pd.read_csv('/Users/coopermigden/data_sets/vehicles_us _1_.csv')
+data = pd.read_csv('/Users/coopermigden/data_sets/vehicles_us.csv')
 #Brings the data into the program
 
 
